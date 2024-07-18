d1fe77eb65a4697eece20eb3f115f4153ae17935
Israel Guia <israelguia19@gmail.com>
2024-07-17 21:54:27 -0300

    Opção Adicionada
    Agregada a Opção de sair para o usuario final.

--- RegistroIDE 4.cpp
+++ RegistroIDE 4.cpp
@@ -156,6 +156,7 @@ int main ()
 	printf ("\t1 - Registrar nomes\n") ;
 	printf ("\t2 - Consultar os nomes\n") ;
 	printf ("\t3 - deletar os nomes \n \n") ;  // Final do Menu
+	printf ("\t4 - sair do sistema \n \n");
 	
 	printf ("Opção:") ;
 	
@@ -179,6 +180,11 @@ int main ()
          	
          	case 3:
             deletar();
+	        break;
+	        
+	        case 4:
+	        printf ("Obrigado por usar este sistema");
+	        return 0;
 	        break;
 	        
 	      
